a251bf0c8986c9a73da0e496f770d892f29650ab
Nikhil Khandelwal <55945199+Nikhil15800@users.noreply.github.com>
2022-05-18 10:12:10 +0530

    Add files via upload

---
+++ rock_papper_scissors.py
@@ -0,0 +1,44 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+Options = ["rock","paper","scissors"]
+
+while True:
+
+    user_input = input("Type rock/paper/Scissors or Q to quit : ").lower()
+    if user_input == "q":
+        break
+
+    if user_input not in Options:
+        continue
+
+    random_number = random.randint(0,2)
+    # rock: 0, paper: 1, scissors:2 
+    computer_pick = Options[random_number]
+    print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You Won!")
+        user_wins +=1
+        
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You Won!")
+        user_wins +=1
+        
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You Won!")
+        user_wins +=1
+    
+    else:
+        print("You lost")
+        computer_wins +=1
+
+
+print("You won", user_wins, "Times.")
+print("The computer won", computer_wins, "Times.")
+
+print("Goodbye!")
